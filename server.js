60d2cc490afd4f3532b9af8e9cfa0a4c237ca994
Ricardo Rivas G <richistron@gmail.com>
2014-12-08 12:16:51 -0600

    conelio v0.10.0 beta added

--- server.js
+++
@@ -1,9 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-
-var app = connect();
-
-app.use(serveStatic('build', {'index': ['index.html', '404.html']}))
-
-console.log('http://localhost:5000');
-app.listen(5000);
